8e656adfd142f750dea7c25215154d4effe58a1e
AlvitoSR <alvarosouzaribeiro.040406@gmail.com>
2026-04-21 20:18:15 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
-// Baseado no template oficial recloudstream/cloudstream-extensions-template
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 buildscript {
     repositories {
         google()
         mavenCentral()
         maven("https://jitpack.io")
     }
-
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-        // Plugin do CloudStream que faz tudo funcionar
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
@@ -22,8 +23,8 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: com.lagradost.cloudstream3.gradle.CloudstreamExtension.() -> Unit) =
-    extensions.getByName<com.lagradost.cloudstream3.gradle.CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
 fun Project.android(configuration: com.android.build.gradle.BaseExtension.() -> Unit) =
     extensions.getByName<com.android.build.gradle.BaseExtension>("android").configuration()
@@ -34,28 +35,28 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "seu-usuario/embedtv-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
     }
 
     android {
-        compileSdkVersion(33)
+        namespace = "com.embedtv"
+        compileSdkVersion(35)
         defaultConfig {
             minSdk = 21
-            targetSdk = 33
+            targetSdk = 35
         }
-
         compileOptions {
             sourceCompatibility = JavaVersion.VERSION_1_8
             targetCompatibility = JavaVersion.VERSION_1_8
         }
-
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = "1.8"
-                freeCompilerArgs = freeCompilerArgs +
-                        "-Xno-call-assertions" +
-                        "-Xno-param-assertions" +
-                        "-Xno-receiver-assertions"
+        tasks.withType<KotlinJvmCompile> {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_1_8)
+                freeCompilerArgs.addAll(
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
+                )
             }
         }
     }
@@ -63,13 +64,10 @@ subprojects {
     dependencies {
         val apk by configurations
         val implementation by configurations
-
-        // Stubs do CloudStream (necessário para compilar)
         apk("com.lagradost:cloudstream3:pre-release")
-
         implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP
-        implementation("org.jsoup:jsoup:1.18.3")              // HTML parser
+        implementation("com.github.Blatzar:NiceHttp:0.4.11")
+        implementation("org.jsoup:jsoup:1.18.3")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
     }
 }
